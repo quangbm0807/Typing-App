119da4b3d7ef6f35b4bd3d3fbc6f7e293a98e823
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-12-10 11:38:24 +0700

    Feat MutilLangugae

--- typing-app/src/App.tsx
+++ typing-app/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Layout } from 'antd';
 import TypingChallenge from './Component/TypingChallenge';
-
-const { Header, Content } = Layout;
-
+import i18n from './language/i18n';
+import { I18nextProvider } from 'react-i18next';
 const App: React.FC = () => {
   return (
-    <TypingChallenge />
+    <I18nextProvider i18n={i18n}>
+      <TypingChallenge />
+    </I18nextProvider>
   );
 };
 
